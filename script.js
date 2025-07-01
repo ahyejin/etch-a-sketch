a97acfbc8268ff8f80b81cd4dbfa3c9d58377836
ahyejin <anishakj8293@gmail.com>
2025-07-01 20:29:00 +0530

    implement multicolor, eraser and clear buttons

--- script.js
+++ script.js
@@ -4,6 +4,48 @@ gridSize.addEventListener("input", function (e) {
     createGrid(e.target.value);
 })
 const color = document.querySelector("#color");
+const multicolor = document.querySelector(".multicolor");
+
+let randomColorFlag = false;
+let shading = false;
+let eraser = false;
+let isClear = false;
+
+color.addEventListener("input", function (e) {
+    buttons.forEach(item => item.style.background = "#edede9");
+    randomColorFlag = false;
+    shading = false;
+    eraser = false;
+
+})
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(item => item.addEventListener("click", function (e) {
+    buttons.forEach(item => item.style.background = "#edede9");
+    e.target.style.background = "#a8dadc";
+    if (e.target.textContent == "Multicolor") {
+        randomColorFlag = true;
+        eraser = false;
+        shading = false;
+    }
+    else if (e.target.textContent == "Shading") {
+        shading = true;
+        randomColorFlag = false;
+        eraser = false;
+    }
+    else if (e.target.textContent == "Eraser") {
+        eraser = true;
+        randomColorFlag = false;
+        shading = false;
+    }
+    else if (e.target.textContent == "Clear") {
+        randomColorFlag = false;
+        shading = false;
+        eraser = false;
+        createGrid(gridSize.value);
+    }
+
+}))
 function createGrid(size) {
     if (sketchpad.hasChildNodes()) {
         sketchpad.textContent = "";
@@ -17,14 +59,31 @@ function createGrid(size) {
             rows.appendChild(squares);
             squares.classList.add("columns");
             squares.addEventListener("mouseover", function (e) {
+                if (randomColorFlag) {
+                    e.target.style.background = randomColor();
+                }
+                else if (eraser) {
+                    e.target.style.background = "#ecf39e";
+            
+                }
+                else {
+                    buttons.forEach(item => item.style.background = "#edede9");
                     e.target.style.background = color.value;
+                }
+
             })
         }
         sketchpad.appendChild(rows);
     }
-    
-}
 
+}
+function randomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+
+}
 createGrid(16);
 
 
